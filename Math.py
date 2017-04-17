fc654e3315ff126b7d84c08948a716e99af7820f
mitalikaushal <mitali.kaushal9@gmail.com>
2017-04-18 03:45:38 +0530

    math functions practice

---
+++ Math.py
@@ -0,0 +1,23 @@
+'''
+Created on 18-Apr-2017
+
+@author: Linkin Park_2
+'''
+# single line comment
+print('hi')
+#print hey
+''' ok 
+hey 
+sorry
+bye 
+'''
+1+3
+print(1+3)
+print(10-2)
+print(2-10)
+print(2*19)
+print(10/2)
+print(2/10)
+print(5/2)
+print(5/34)
+print(4**4)
