da0124b03b00b08c473a86c4cc934d64509b53b3
mitalikaushal <mitali.kaushal9@gmail.com>
2017-04-18 03:35:28 +0530

    diffrent styles of printing

---
+++ Basic.py
@@ -0,0 +1,13 @@
+'''
+Created on 18-Apr-2017
+
+@author: Linkin Park_2
+'''
+print(3+4)
+print('This is an example of print function')
+print('We\'are going to the store')
+print('Hi'+' there')
+print('hey','you')
+print('hi',5)
+print('hi'+str(5))
+print(float('5.2')+2)
